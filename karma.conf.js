63e1ea477aa03a3c4965bd971e25d6e38d0f8bc2
Zachary Cauchi <zach.cauchi@hotmail.com>
2022-06-14 23:08:01 +0200

    fix: web page not being hosted correctly

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/todo-app'),
+      dir: require('path').join(__dirname, './coverage/angular-sample-todo-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
